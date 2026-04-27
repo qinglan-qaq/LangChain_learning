6cfddc83ed8269f12109709573e995e0020dfb8d
qinglan-qaq <1710202731@qq.com>
2026-04-27 16:41:30 +0800

    构建CRAG的基本框架 但是初始化和评估器需要再考虑

---
+++ lawApp_LangGraph/RAG_service/CRAG.py
@@ -0,0 +1,17 @@
+class CRAGService:
+    def __init__(self):
+        pass
+
+    def Evaluate(self, query):
+
+        pass
+
+    def correct_process(self, query):
+        pass
+
+    def incorrect_process(self, query):
+        pass
+
+    def ambiguous_process(self, query):
+        pass
+
